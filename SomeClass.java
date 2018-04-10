9ae5561926a7876d1e8bfd99d09ef6bd925728e3
Tony <tlam@guidewire.com>
2018-04-10 08:39:47 -0700

    Adding SomeClass

---
+++ SomeClass.java
@@ -0,0 +1,19 @@
+package something
+
+import something;
+
+
+public class SomeClass{
+
+	private something;
+	private somethingTwo;
+
+	public String doSomething{} {
+
+	String someVariable = "";
+	ArrayList<String> someArrayList = new ArrayList<String>;
+	
+	return "";
+
+	}
+}
